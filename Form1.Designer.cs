6f7528e1971b8c695afd5505f533514de77885da
Татьяна Кожевникова <tatanakozevnikova786@gmail.com>
2024-10-10 21:22:30 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 29);
+            this.textBox1.TabIndex = 0;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 82);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 29);
+            this.textBox2.TabIndex = 1;
+            //this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(23, 142);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(239, 29);
+            this.textBox3.TabIndex = 2;
+            //this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(337, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(36, 29);
+            this.panel1.TabIndex = 3;
+            //this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(337, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(36, 29);
+            this.panel2.TabIndex = 4;
+            //this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(337, 142);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(36, 29);
+            this.panel3.TabIndex = 5;
+            //this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace Ping
+{
+    public partial class Form1 : Form
+    {
+        Process calcProcess, cmdProcess, wordProcess, excelProcess, ieProcess, browserProcess;
+        List<Process> processes;
+        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 120000;
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            await CheckNodeAvailability(textBox1.Text, "out1.txt", panel1);
+            await CheckNodeAvailability(textBox2.Text, "out2.txt", panel2);
+            await CheckNodeAvailability(textBox3.Text, "out3.txt", panel3);
+        }
+
+        private async void Timer1_Tick(object sender, EventArgs e)
+        {
+            //await CheckNodeAvailability(textBox1.Text, panel1);
+            //await CheckNodeAvailability(textBox2.Text, panel2);
+            //await CheckNodeAvailability(textBox3.Text, panel3);
+        }
+
+        private async Task CheckNodeAvailability(string address, string outputFile, Panel statusPanel)
+        {
+            // Формируем команду для запуска
+            string pingCommand = $"/C ping {address} > {outputFile}";
+
+            // Вызываем командную строку и запускаем пинг
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",  // Запускаем командную строку
+                    Arguments = pingCommand,  // Передаем команду пинг с перенаправлением вывода в файл
+                    RedirectStandardOutput = false,  // Вывод не нужен
+                    UseShellExecute = true,  // Выполняем в системной оболочке
+                    CreateNoWindow = true  // Без окна консоли
+                }
+            };
+
+            try
+            {
+                process.Start();
+                await process.WaitForExitAsync();
+
+                // Проверяем результат пинга через чтение файла
+                string result = File.ReadAllText(outputFile);
+
+                if (result.Contains("TTL="))  // Если пинг успешен
+                {
+                    statusPanel.BackColor = Color.Green;  // Узел доступен
+                }
+                else
+                {
+                    statusPanel.BackColor = Color.Red;  // Узел недоступен
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при пинге узла {address}: {ex.Message}");
+                statusPanel.BackColor = Color.Red;  // В случае ошибки
+            }
+        }
+    }
+}
